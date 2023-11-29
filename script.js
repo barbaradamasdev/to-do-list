0082c08971680638768e4713f3caf8d20d39042f
Barbara Damasceno <barbaradamas.dev@gmail.com>
2023-11-29 19:27:32 -0300

    create fetch js and json file

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-let id = 0;
-let novaTarefa = [];
-
-function addTarefa () {
-    id++
-    let task_nome = document.getElementById('task_nome');
-    let task_data_inicio = document.getElementById('task_data_inicio');
-    let task_hora_inicio = document.getElementById('task_hora_inicio');
-    let task_data_termino = document.getElementById('task_data_termino');
-    let task_hora_termino= document.getElementById('task_hora_termino');
-    let task_descricao = document.getElementById('task_descricao');
-
-    novaTarefa = [task_nome, task_data_inicio, task_hora_inicio, task_data_termino, task_hora_termino, task_descricao];
-    console.table(novaTarefa);
-}
+// Pega os dados do json e add na tabela
+fetch('usuarios.json')
+.then(arquivo => arquivo.json())
+.then(arquivoJson => {
+    arquivoJson.usuarios.forEach(function (usuario) {
+        var tarefas = usuario.tarefas;
+        tarefas.forEach(function (tarefa) {
+            updateTable(usuario.nome, usuario.email, tarefa);
+        });
+    });
+})
+.catch(error => console.error('Error fetching data:', error));
