b57c0fde73322559a79b75f1e9796400ff91595b
Barbara Damasceno <barbaradamas.dev@gmail.com>
2023-11-29 19:34:22 -0300

    add addTarefa function

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ fetch('usuarios.json')
     });
 })
 .catch(error => console.error('Error fetching data:', error));
+
+
+// Adicionar a tarefa ao JSON e atualiza tabela
+let novaTarefa = [];
+let usuario = "john@example.com";
+
+function addTarefa () {
+    let task_nome = document.getElementById('task_nome').value;
+    let task_data_inicio = document.getElementById('task_data_inicio').value;
+    let task_hora_inicio = document.getElementById('task_hora_inicio').value;
+    let task_data_termino = document.getElementById('task_data_termino').value;
+    let task_hora_termino= document.getElementById('task_hora_termino').value;
+    let task_descricao = document.getElementById('task_descricao').value;
+
+    novaTarefa = [task_nome, task_data_inicio, task_hora_inicio, task_data_termino, task_hora_termino, task_descricao];
+    console.log(novaTarefa);
+
+    //userData.usuarios.
+    updateTable(usuario.nome, usuario.email, tarefa);
+}
