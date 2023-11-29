c458dea62d8834c312e75fd36af95a67da5aa5d7
Barbara Damasceno <barbaradamas.dev@gmail.com>
2023-11-29 19:34:36 -0300

    add formatDate

--- script.js
+++ script.js
@@ -30,3 +30,8 @@ function addTarefa () {
     //userData.usuarios.
     updateTable(usuario.nome, usuario.email, tarefa);
 }
+
+function formatDate(dateString) {
+    const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+    return new Date(dateString).toLocaleDateString('pt-BR', options);
+}
